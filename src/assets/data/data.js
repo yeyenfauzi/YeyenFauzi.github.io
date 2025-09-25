e2bfc97d203f925c4357bf304c5ab013f48a4628
yeyenfauzi <akuntata@gmail.com>
2025-09-25 09:49:31 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -7,6 +7,8 @@ export const data = {
             father: 'Ebeng Satibi (Alm.)',
             mother: 'Siti Suadah',
             image: './src/assets/images/cewe.png'
+            instagram: 'https://www.instagram.com/ysulistari/',
+            profileImage: './src/assets/images/instagram.png'
         },
         P: {
             id: 2,
@@ -15,6 +17,8 @@ export const data = {
             father: 'Hermanto (Alm.)',
             mother: 'Titik Kartinah',
             image: './src/assets/images/cowo.png'
+            instagram: 'https://www.instagram.com/ozhie_/',
+            profileImage: './src/assets/images/instagram.png' 
         },
 
         couple: './src/assets/images/couple.png'
